d8937732ec9f6d765dc82bc82141014efff25ff3
Liang Wang <wang.leon@gmail.com>
2019-10-25 15:39:20 +0800

    add a doc

--- naoctest/subpack1/fitting.py
+++ naoctest/subpack1/fitting.py
@@ -2,11 +2,37 @@ import os
 import re
 import datetime
 
+def test(a, b):
+    """
+    This function adds a and b.
+
+    Args:
+        a (int or float): input parameter 1.
+        b (int or float): input parameter 2.
+
+    Returns:
+        c: sum of a and b.
+
+    """
+
+    # add a +b 
+    return a + b
+
 class Fitting(object):
+    """TEST class for least square fitting.
+
+    xxxx a  a
+    """
     def __init__(self):
+        """
+        constructor for fitting.
+        """
         self.name = 'test'
         self.data = 10
         self.data2 = 'apple'
 
     def add_data(self):
+        """ Add data.
+
+        """
         self.data = self.data + 1
