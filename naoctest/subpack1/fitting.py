922204d11c0484790a0fd525504984b8b4baaf06
Liang Wang <wang.leon@gmail.com>
2019-10-25 14:43:09 +0800

    change Fitting class

--- naoctest/subpack1/fitting.py
+++ naoctest/subpack1/fitting.py
@@ -6,6 +6,7 @@ class Fitting(object):
     def __init__(self):
         self.name = 'test'
         self.data = 10
+        self.data2 = 'apple'
 
     def add_data(self):
         self.data = self.data + 1
